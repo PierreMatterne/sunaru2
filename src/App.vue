e8a132d631d5ddbbc1adb3b37ed6f28abdd55a42
Matterne Pierre <pierre.matterne@highco.be>
2022-01-12 15:17:08 +0100

    add the first files to the project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <compas :direction="direction" />
+  <window />
+  <main-input />
+  <inventory /> 
+</template>
+
+<script>
+  import Compas from './components/Compas.vue';
+  import Inventory from './components/Inventory.vue';
+  import MainInput from './components/MainInput.vue';
+  import Window from './components/Window.vue';
+
+  export default {
+    name: 'App',
+    data(){
+      return {
+        direction: 'N',
+      }
+    },
+    components: {
+      Compas,
+      Inventory,
+      MainInput,
+      Window,
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Compas.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		Compas point to: {{direction}}
+	</div>
+</template>
+
+<!-- ================================================================================== -->
+
+<script>
+	export default {
+		name:"",
+		props: {
+			direction:{
+				type: String,
+				required: true
+			}
+		}
+	}
+</script>
+
+<!-- ================================================================================== -->
+
+<style lang="scss" scoped>
+	
+</style>
